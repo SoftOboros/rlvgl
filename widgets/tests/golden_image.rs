5a7fb601f052bf2b00a683047f2e95714f60443b
Ira Abbott <ira@mycloud.blue>
2025-07-27 22:05:07 -0400

    Add dark theme and theme application tests

---
+++ widgets/tests/golden_image.rs
@@ -0,0 +1,46 @@
+use platform::display::{BufferDisplay, DisplayDriver};
+use rlvgl_core::renderer::Renderer;
+use rlvgl_core::widget::{Color, Rect, Widget};
+use rlvgl_widgets::image::Image;
+
+struct DisplayRenderer<'a> {
+    display: &'a mut BufferDisplay,
+}
+
+impl<'a> Renderer for DisplayRenderer<'a> {
+    fn fill_rect(&mut self, rect: Rect, color: Color) {
+        let colors = vec![color; (rect.width * rect.height) as usize];
+        self.display.flush(rect, &colors);
+    }
+
+    fn draw_text(&mut self, _pos: (i32, i32), _text: &str, _color: Color) {}
+}
+
+#[test]
+fn image_render() {
+    let mut display = BufferDisplay::new(2, 2);
+    let mut renderer = DisplayRenderer {
+        display: &mut display,
+    };
+
+    let pixels = [
+        Color(1, 0, 0),
+        Color(0, 1, 0),
+        Color(0, 0, 1),
+        Color(1, 1, 1),
+    ];
+    let image = Image::new(
+        Rect {
+            x: 0,
+            y: 0,
+            width: 2,
+            height: 2,
+        },
+        2,
+        2,
+        &pixels,
+    );
+    image.draw(&mut renderer);
+
+    assert_eq!(display.buffer, pixels);
+}
---
+++ widgets/tests/golden_slider.rs
@@ -0,0 +1,49 @@
+use platform::display::{BufferDisplay, DisplayDriver};
+use rlvgl_core::renderer::Renderer;
+use rlvgl_core::widget::{Color, Rect, Widget};
+use rlvgl_widgets::slider::Slider;
+
+struct DisplayRenderer<'a> {
+    display: &'a mut BufferDisplay,
+}
+
+impl<'a> Renderer for DisplayRenderer<'a> {
+    fn fill_rect(&mut self, rect: Rect, color: Color) {
+        let colors = vec![color; (rect.width * rect.height) as usize];
+        self.display.flush(rect, &colors);
+    }
+
+    fn draw_text(&mut self, _pos: (i32, i32), _text: &str, _color: Color) {}
+}
+
+#[test]
+fn slider_render() {
+    let mut display = BufferDisplay::new(20, 10);
+    let mut renderer = DisplayRenderer {
+        display: &mut display,
+    };
+
+    let mut slider = Slider::new(
+        Rect {
+            x: 0,
+            y: 0,
+            width: 20,
+            height: 10,
+        },
+        0,
+        10,
+    );
+    slider.style.bg_color = Color(1, 1, 1);
+    slider.style.border_color = Color(2, 2, 2);
+    slider.knob_color = Color(3, 3, 3);
+    slider.set_value(5);
+    slider.draw(&mut renderer);
+
+    // knob center pixel
+    assert_eq!(display.buffer[5 * 20 + 10], Color(3, 3, 3));
+    // track pixel outside knob
+    let track_y = 0 + (10 - 4) / 2 + 1; // middle of track
+    assert_eq!(display.buffer[track_y as usize * 20 + 2], Color(2, 2, 2));
+    // background pixel above track
+    assert_eq!(display.buffer[0 * 20 + 0], Color(1, 1, 1));
+}
