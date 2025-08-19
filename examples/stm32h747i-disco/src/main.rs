4b99700a94a1d6c000bbc2a8794238ec572e51aa
Ira Abbott <ira@mycloud.blue>
2025-08-19 17:48:15 -0400

    examples: wire stm32h747i-disco button input

--- examples/stm32h747i-disco/src/main.rs
+++ examples/stm32h747i-disco/src/main.rs
@@ -18,7 +18,6 @@ use panic_halt as _;
 
 #[path = "../../common_demo/lib.rs"]
 mod common_demo;
-use common_demo::build_demo;
 
 /// Global allocator backed by a fixed-size heap in RAM.
 #[global_allocator]
@@ -39,10 +38,163 @@ fn main() -> ! {
         ALLOC.init(start, HEAP_SIZE);
     }
 
-    // TODO: initialize `Stm32h747iDiscoDisplay` with a concrete blitter and
-    // touch input driver once the hardware integration is complete.
-    let _demo = build_demo(480, 272);
+    #[cfg(all(
+        feature = "stm32h747i_disco",
+        any(target_arch = "arm", target_arch = "aarch64")
+    ))]
+    {
+        use core::convert::Infallible;
+        use embedded_hal::{
+            digital::{ErrorType as DigitalError, InputPin, OutputPin},
+            i2c::{ErrorType as I2cError, I2c, Operation, SevenBitAddress},
+            pwm::{ErrorType as PwmError, SetDutyCycle},
+        };
+        use rlvgl::core::event::{Event, Key};
+        use rlvgl::platform::{
+            CpuBlitter, InputDevice, Stm32h747iDiscoDisplay, Stm32h747iDiscoInput,
+        };
+        use stm32h7::stm32h747cm7::{DSIHOST, FMC, LTDC, RCC};
 
+        struct DummyBacklight;
+        impl PwmError for DummyBacklight {
+            type Error = Infallible;
+        }
+        impl SetDutyCycle for DummyBacklight {
+            fn set_duty_cycle(&mut self, _duty: u16) -> Result<(), Self::Error> {
+                Ok(())
+            }
+            fn max_duty_cycle(&self) -> u16 {
+                0
+            }
+        }
+
+        struct DummyReset;
+        impl DigitalError for DummyReset {
+            type Error = Infallible;
+        }
+        impl OutputPin for DummyReset {
+            fn set_high(&mut self) -> Result<(), Self::Error> {
+                Ok(())
+            }
+            fn set_low(&mut self) -> Result<(), Self::Error> {
+                Ok(())
+            }
+        }
+
+        struct DummyI2c;
+        impl I2cError for DummyI2c {
+            type Error = Infallible;
+        }
+        impl I2c<SevenBitAddress> for DummyI2c {
+            fn read(
+                &mut self,
+                _address: SevenBitAddress,
+                _buf: &mut [u8],
+            ) -> Result<(), Self::Error> {
+                Ok(())
+            }
+            fn write(
+                &mut self,
+                _address: SevenBitAddress,
+                _bytes: &[u8],
+            ) -> Result<(), Self::Error> {
+                Ok(())
+            }
+            fn write_read(
+                &mut self,
+                _address: SevenBitAddress,
+                _bytes: &[u8],
+                _buf: &mut [u8],
+            ) -> Result<(), Self::Error> {
+                Ok(())
+            }
+            fn transaction(
+                &mut self,
+                _address: SevenBitAddress,
+                _ops: &mut [Operation<'_>],
+            ) -> Result<(), Self::Error> {
+                Ok(())
+            }
+        }
+
+        struct DummyButton;
+        impl DigitalError for DummyButton {
+            type Error = Infallible;
+        }
+        impl InputPin for DummyButton {
+            fn is_high(&mut self) -> Result<bool, Self::Error> {
+                Ok(false)
+            }
+            fn is_low(&mut self) -> Result<bool, Self::Error> {
+                Ok(true)
+            }
+        }
+
+        struct ButtonInput<B: InputPin> {
+            button: B,
+            last: bool,
+        }
+        impl<B: InputPin> ButtonInput<B> {
+            fn new(button: B) -> Self {
+                Self {
+                    button,
+                    last: false,
+                }
+            }
+        }
+        impl<B: InputPin> InputDevice for ButtonInput<B> {
+            fn poll(&mut self) -> Option<Event> {
+                let pressed = self.button.is_low().ok()?;
+                match (pressed, self.last) {
+                    (true, false) => {
+                        self.last = true;
+                        Some(Event::KeyDown { key: Key::Enter })
+                    }
+                    (false, true) => {
+                        self.last = false;
+                        Some(Event::KeyUp { key: Key::Enter })
+                    }
+                    _ => None,
+                }
+            }
+        }
+
+        let blitter = CpuBlitter;
+        let backlight = DummyBacklight;
+        let reset = DummyReset;
+        let ltdc: LTDC = unsafe { core::mem::MaybeUninit::uninit().assume_init() };
+        let dsi: DSIHOST = unsafe { core::mem::MaybeUninit::uninit().assume_init() };
+        let fmc: FMC = unsafe { core::mem::MaybeUninit::uninit().assume_init() };
+        let mut rcc: RCC = unsafe { core::mem::MaybeUninit::uninit().assume_init() };
+        let mut _display =
+            Stm32h747iDiscoDisplay::new(blitter, backlight, reset, ltdc, dsi, fmc, &mut rcc);
+        let i2c = DummyI2c;
+        let mut input = Stm32h747iDiscoInput::new(i2c);
+        let button = DummyButton;
+        let mut button_input = ButtonInput::new(button);
+
+        let demo = common_demo::build_demo(800, 480);
+        let root = demo.root;
+        let pending = demo.pending;
+        let to_remove = demo.to_remove;
+
+        loop {
+            if let Some(evt) = input.poll() {
+                root.borrow_mut().dispatch_event(&evt);
+                common_demo::flush_pending(&root, &pending, &to_remove);
+            }
+            if let Some(evt) = button_input.poll() {
+                root.borrow_mut().dispatch_event(&evt);
+                common_demo::flush_pending(&root, &pending, &to_remove);
+            }
+            cortex_m::asm::nop();
+        }
+    }
+
+    #[cfg(not(all(
+        feature = "stm32h747i_disco",
+        any(target_arch = "arm", target_arch = "aarch64")
+    )))]
     loop {
         cortex_m::asm::nop();
     }
